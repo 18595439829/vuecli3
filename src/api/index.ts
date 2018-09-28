737359ecbb5c1c080d1fc49250527991efef145b
郝洋洋 <1622072353@qq.com>
2018-09-28 10:37:49 +0800

    同步代码

---
+++ src/api/index.ts
@@ -0,0 +1,12 @@
+import axios from 'axios'
+axios.defaults.baseURL = '/api'
+axios.defaults.headers.post['Content-Type'] =
+  'application/x-www-form-urlencoded;charset=UTF-8' // 配置请求头
+export default {
+  tulingGET(data: string) {
+    return axios.get('' + data)
+  },
+  tulingPOST(data: JSON) {
+    return axios.post('', data)
+  }
+}
