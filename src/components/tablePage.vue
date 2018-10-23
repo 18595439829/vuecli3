1d969511188cfff8389ae00e4a865ae9bd706548
郝洋洋 <1622072353@qq.com>
2018-10-23 16:21:40 +0800

    组件封装

---
+++ src/components/tablePage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Select v-model="selectValue"  @on-change="selectChange" :label-in-value="true">
+            <Option v-for="item in options" :value="item.value" :key="item.value">{{item.label}}</Option>
+        </Select>
+        <Table :columns="columns" :data="data"></Table>
+        <Page :total="total" show-sizer @on-change="pageChange" @on-page-size-change="sizeChange"/>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+        selectValue: this.selectDefault
+    };
+  },
+  props: [ "selectDefault","selectChange","options", "total", "columns", "data", "pageChange", "sizeChange"],
+  methods: {
+     
+  }
+};
+</script>
--- src/views/echarts.vue
+++ src/views/echarts.vue
@@ -2,15 +2,20 @@
   <div>
     <div ref="echart"
          style="width: 600px;height:400px;"></div>
+    <table-page :selectDefault="selectDefault" :options="options" :selectChange="selectChange" :pageChange="pageChange" :sizeChange="sizeChange"  :total="100" :columns="columns1" :data="data1"></table-page>
   </div>
 </template>
 <script>
+import tablePage from "../components/tablePage";
 export default {
+  components: {
+    tablePage
+  },
   data() {
     return {
       option: {
         tooltip: {
-          formatter: '{a} <br/>{b} : {c}%'
+          formatter: "{a} <br/>{b} : {c}%"
         },
         toolbox: {
           feature: {
@@ -20,22 +25,104 @@ export default {
         },
         series: [
           {
-            name: '业务指标',
-            type: 'gauge',
-            detail: { formatter: '{value}%' },
-            data: [{ value: 50, name: '完成率' }]
+            name: "业务指标",
+            type: "gauge",
+            detail: { formatter: "{value}%" },
+            data: [{ value: 50, name: "完成率" }]
           }
         ]
-      }
+      },
+      selectDefault: "Sydney",
+      options: [
+        {
+          value: "New York",
+          label: "New York"
+        },
+        {
+          value: "London",
+          label: "London"
+        },
+        {
+          value: "Sydney",
+          label: "Sydney"
+        },
+        {
+          value: "Ottawa",
+          label: "Ottawa"
+        },
+        {
+          value: "Paris",
+          label: "Paris"
+        },
+        {
+          value: "Canberra",
+          label: "Canberra"
+        }
+      ],
+      columns1: [
+        {
+          title: "Name",
+          key: "name"
+        },
+        {
+          title: "Age",
+          key: "age"
+        },
+        {
+          title: "Address",
+          key: "address"
+        }
+      ],
+      data1: [
+        {
+          name: "John Brown",
+          age: 18,
+          address: "New York No. 1 Lake Park",
+          date: "2016-10-03"
+        },
+        {
+          name: "Jim Green",
+          age: 24,
+          address: "London No. 1 Lake Park",
+          date: "2016-10-01"
+        },
+        {
+          name: "Joe Black",
+          age: 30,
+          address: "Sydney No. 1 Lake Park",
+          date: "2016-10-02"
+        },
+        {
+          name: "Jon Snow",
+          age: 26,
+          address: "Ottawa No. 2 Lake Park",
+          date: "2016-10-04"
+        }
+      ],
+      pageNum: 1,
+      pageSize: 10
+    };
+  },
+  methods: {
+    pageChange(page) {
+      console.log("页码", page);
+      console.log("假装掉接口", this.pageNum, this.pageSize);
+    },
+    sizeChange(size) {
+      console.log("每页多少条", size);
+      console.log("假装掉接口", this.pageNum, this.pageSize);
+    },
+    selectChange(value) {
+      console.log(value)
     }
   },
   mounted() {
-    if (this.option && typeof this.option === 'object') {
-      console.log('aaa', this.$refs.echart)
-      this.$eCharts.init(this.$refs.echart).setOption(this.option, true)
+    if (this.option && typeof this.option === "object") {
+      console.log("aaa", this.$refs.echart);
+      this.$eCharts.init(this.$refs.echart).setOption(this.option, true);
     }
   }
-}
+};
 </script>
 <style>
 </style>
