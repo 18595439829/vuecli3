b3d9b7a6f468670caac30a7742851781bd0f9c5a
郝洋洋 <1622072353@qq.com>
2018-10-31 11:27:31 +0800

    自定义过滤器,千位分隔符

--- src/views/aplayer.vue
+++ src/views/aplayer.vue
@@ -1,18 +1,19 @@
 <template>
-    <div>
-        <div style="padding:10px 0;">
-            <aplayer autoplay :music="musicList">
-            </aplayer>
-        </div>
- 
+  <div>
+    <div style="padding:10px 0;">
+      <aplayer autoplay
+               :music="musicList">
+      </aplayer>
     </div>
+    {{number | capitalize}}
+  </div>
 </template>  
   
 <script>
-import aplayer from "vue-aplayer";
- 
+import aplayer from 'vue-aplayer'
+
 export default {
-  name: "Aplayer",
+  name: 'Aplayer',
   components: {
     //别忘了引入组件
     aplayer
@@ -20,22 +21,44 @@ export default {
   data() {
     return {
       musicList: {
-        title: "youkao100",
+        title: 'youkao100',
         src: 'http://172.18.238.202:7788/voice/failed11.mp3',
         artist: ' ',
-        pic: ""
+        pic: ''
         // lrc: "[00:00.00]lrc here\n[00:01.00]aplayer"
-      }
-    };
-  },
-  computed: {
-    url() {
-      return this.pdfurl;
+      },
+      number: 1000000000.1001
     }
   },
-  mounted() {
-  }
-};
+  filters: {
+    capitalize: num => {
+      num = num.toString().split('.') // 分隔小数点
+      var arr = num[0].split('').reverse() // 转换成字符数组并且倒序排列
+      var res = []
+      for (var i = 0, len = arr.length; i < len; i++) {
+        if (i % 3 === 0 && i !== 0) {
+          res.push(',') // 添加分隔符
+        }
+        res.push(arr[i])
+      }
+      res.reverse() // 再次倒序成为正确的顺序
+      if (num[1]) {
+        // 如果有小数的话添加小数部分
+        res = res.join('').concat('.' + num[1])
+      } else {
+        res = res.join('')
+      }
+      return res
+    }
+  },
+  methods: {},
+  computed: {
+    url() {
+      return this.pdfurl
+    }
+  },
+  mounted() {}
+}
 </script>  
   
 <style>
