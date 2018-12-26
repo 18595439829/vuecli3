ffbb67da098209fffbfa3c777c19c3aaf0615243
郝洋洋 <1622072353@qq.com>
2018-12-26 10:06:36 +0800

    使用拼音插件

---
+++ src/views/pinYin/chinese.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>
+      {{'你好,世界,埠,斛' | pinyin}}
+    </div>
+  </div>
+</template>
+<script>
+import pinYin from 'pinyin'
+export default {
+  name: 'chinese',
+  filters: {
+    pinyin(value) {
+      if (value) {
+        let pinyinArray = pinYin(value, { style: pinYin.STYLE_TONE })
+        let str = ''
+        pinyinArray.forEach(item => {
+           str = `${str}  ${item} `
+        });
+        return `${value}(${str})` 
+      } else {
+        return ''
+      }
+    }
+  }
+}
+</script>
---
+++ src/views/pinYin/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="router">
+      <router-link :to="{name: 'chinese'}">汉语拼音</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  name: 'index'
+}
+</script>
+<style lang="less">
+  
+</style>
