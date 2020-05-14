c20867cec1722955afc13d59942319ee8a2a53b7
haoyangyang <1622072353@qq.com>
2020-05-14 16:28:50 +0800

    feat: add deploy branch

---
+++ js/chunk-74bd4c17.d04558e6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-74bd4c17"],{"95c9":function(n,c,w){}}]);
+//# sourceMappingURL=chunk-74bd4c17.d04558e6.js.map
--- src/typescript/greeter.ts
+++
@@ -1,7 +0,0 @@
-function greeter(person: string) {
-  return 'Hello, ' + person
-}
-
-var user = 'Jane User'
-
-document.body.innerHTML = greeter(user)
