ea6142e81ab4f0da0ec93c97f7a7c6c9a051727f
haoyangyang <wb.haoyangyang@mesg.corp.netease.com>
2021-10-12 17:14:50 +0800

    feat(color-picker): 添加颜色选择器color-picker

--- src/views/color-pikcer/TheIndex.vue
+++ src/views/color-pikcer/TheIndex.vue
@@ -1,21 +1,116 @@
 <template>
   <div :class="$style['container']">
-    color-picker
+    <div :class="$style['list']">
+      <div
+        :class="$style['item']"
+        v-for="item in componentList"
+        :key="item.name"
+      >
+        <div :class="$style['title']">{{ item.name }}</div>
+        <component
+          v-model="color"
+          :class="$style['comp']"
+          :is="item.component"
+          :disableAlpha="true"
+          :disableFields="true"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import {
+  Compact,
+  Grayscale,
+  Twitter,
+  Material,
+  Slider,
+  Swatches,
+  Photoshop,
+  Sketch,
+  Chrome,
+} from "vue-color";
+
 export default {
-  name: 'TheIndex',
+  name: "TheIndex",
+  components: {
+    Compact,
+    Grayscale,
+    Twitter,
+    Material,
+    Slider,
+    Swatches,
+    Photoshop,
+    Sketch,
+    Chrome,
+  },
   data() {
     return {
+      color: "",
+      componentList: [
+        {
+          name: "Compact",
+          component: Compact,
+        },
+        {
+          name: "Grayscale",
+          component: Grayscale,
+        },
+        {
+          name: "Twitter",
+          component: Twitter,
+        },
+        {
+          name: "Material",
+          component: Material,
+        },
+        {
+          name: "Slider",
+          component: Slider,
+        },
+        {
+          name: "Swatches",
+          component: Swatches,
+        },
+        {
+          name: "Photoshop",
+          component: Photoshop,
+        },
+        {
+          name: "Chrome",
+          component: Chrome,
+        },
+      ],
     };
   },
   methods: {},
 };
 </script>
-
+<style lang="less" scoped>
+/deep/ .vc-chrome-color-wrap {
+    display: none;
+}
+</style>
 <style lang="less" module>
- .container {
- }
+.container {
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+    .item {
+      //   width: 200px;
+      //   height: 200px;
+      border: 1px solid #3360ff;
+      border-radius: 6px;
+      box-shadow: 0px 2px 4px #cccccc;
+      margin: 10px 10px 10px 0;
+      .title {
+        padding: 12px;
+        font-size: 16px;
+      }
+      .comp {
+      }
+    }
+  }
+}
 </style>
