775cc18e24ed57054de3677fc7f1f3828b3d0c22
haoyangyang <wb.haoyangyang@mesg.corp.netease.com>
2022-04-06 16:14:25 +0800

    feat(camera): 测试人像识别

--- src/views/http/TheHttp.vue
+++ src/views/http/TheHttp.vue
@@ -1,10 +1,12 @@
 <template>
   <div :class="$style['container']">
-    TheHttp
+    <button @click="getCamera">点击</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'TheHttp',
   data() {
@@ -13,7 +15,20 @@ export default {
   },
   methods: {
     getCamera() {
-      window
+      axios.post(
+          "https://service-4f7hbhda-1258344699.gz.apigw.tencentcs.com/release/sendto_demo_apis",
+          {
+            module: "cms",
+            action: "ImageRecognition",
+            uid: "100003360055",
+            payload: {
+              FileUrl:
+                "https://bda-demo-default-1254418846.cos.ap-guangzhou.myqcloud.com/d4.jpg",
+            },
+          }
+        ).then(res => {
+          console.log(res)
+        });
     }
   },
 };
